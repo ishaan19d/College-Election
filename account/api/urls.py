06a703d1597d8a34f5f097dd38b1da4969b2c08f
ishaan19d <ishaandas848@gmail.com>
2024-05-16 18:06:52 +0530

    Minor nomenclature fixes

--- account/api/urls.py
+++ account/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import EmailSubmissionView, OTPVerificationView, AccountCreationView, StudentLoginView, PhDLoginView, LogoutView
 
 urlpatterns = [
-    path('submit_email/', EmailSubmissionView.as_view(), name='email_submission'),
-    path('verify_otp/', OTPVerificationView.as_view(), name='otp_verification'),
-    path('create_account/', AccountCreationView.as_view(), name='account_creation'),
+    path('register-email/', EmailSubmissionView.as_view(), name='email_registration'),
+    path('verify-otp/', OTPVerificationView.as_view(), name='otp_verification'),
+    path('create-account/', AccountCreationView.as_view(), name='account_creation'),
     path('student-login/', StudentLoginView.as_view(), name='account_login'),
     path('phd-login/', PhDLoginView.as_view(), name='account_login'),
     path('logout/', LogoutView.as_view(), name='account_logout'),
